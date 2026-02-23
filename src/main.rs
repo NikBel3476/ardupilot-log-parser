bc35f18fb9e6bb89b680879c4d142e8de1a3c727
NikBel3476 <nikita.belkin3476@gmail.com>
2026-02-23 18:54:03 +0400

    feature: add new parsing states

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
-use std::io::{Read, Seek};
+use std::io::Read;
 
-const HEAD1: u8 = 163;
-const HEAD2: u8 = 149;
+const HEAD1: u8 = 0xA3;
+const HEAD2: u8 = 0x95;
 const FMT_ATTRIBUTE: u8 = 128;
 
 enum ReadState {
-    AwaitHead,
-    AwaitAttribute,
-    ParseMessage
+    Header(HeaderReadState),
+    ParseMessage(MsgBodyReadState),
+}
+
+enum HeaderReadState {
+    Head1,
+    Head2,
+    MsgType
+}
+
+enum MsgBodyReadState {
+    FormatType(u8),
+    Length(u8),
+    Body(u8)
 }
 
 struct Message {
-    r#type: String,
-    length: u16,
+    msg_type: u8,
+    length: u8,
     name: String,
     format: String,
-    columns: Vec<String>
+    columns: Vec<String>,
 }
 
 // FMT msg
@@ -36,29 +47,73 @@ fn main() {
         .unwrap_or_else(|_| panic!("Cannot open file {log_file_path}"));
     let mut reader = std::io::BufReader::new(log_file);
     let mut buf = vec![];
-    let _ = reader.read_to_end(&mut buf).expect("Failed to read file");
+    let total_bytes = reader.read_to_end(&mut buf).expect("Failed to read file");
 
-    let mut read_state = ReadState::AwaitHead;
-    for byte in buf {
-        read_state = match read_state {
-            ReadState::AwaitHead => {
-                if byte == HEAD1 || byte == HEAD2 {
-                    println!("head found");
-                    ReadState::AwaitAttribute
-                } else {
-                    ReadState::AwaitHead
+    let format_msg = Message {
+        msg_type: HEAD1.into(),
+        length: 89,
+        name: "FMT".to_string(),
+        format: "BBnNZ".to_string(),
+        columns: vec![
+            "Type".to_string(),
+            "Length".to_string(),
+            "Name".to_string(),
+            "Format".to_string(),
+            "Columns".to_string(),
+        ],
+    };
+    let mut fmt = std::collections::HashMap::new();
+    fmt.insert(format_msg.msg_type, format_msg);
+    let mut read_state = ReadState::Header(HeaderReadState::Head1);
+    let mut msg_counter = 0u32;
+    let mut skipped_msgs = 0u32;
+    let mut offset = 0;
+    while offset < total_bytes {
+        let byte = buf[offset];
+        (read_state, offset) = match read_state {
+            ReadState::Header(header_read_state) => match header_read_state {
+                HeaderReadState::Head1 => {
+                    if byte == HEAD1 {
+                        (ReadState::Header(HeaderReadState::Head2), offset + 1)
+                    } else {
+                        (ReadState::Header(HeaderReadState::Head1), offset + 1)
+                    }
+                },
+                HeaderReadState::Head2 => {
+                    if byte == HEAD2 {
+                        msg_counter += 1;
+                        (ReadState::Header(HeaderReadState::MsgType), offset + 1)
+                    } else {
+                        skipped_msgs += 1;
+                        (ReadState::Header(HeaderReadState::Head1), offset + 1)
+                    }
+                },
+                HeaderReadState::MsgType => {
+                    (ReadState::ParseMessage(MsgBodyReadState::FormatType(byte)), offset + 1)
                 }
-            },
-            ReadState::AwaitAttribute => {
-                if byte == FMT_ATTRIBUTE {
-                    ReadState::ParseMessage
-                } else {
-                    ReadState::AwaitHead
+            }
+            ReadState::ParseMessage(msg_body_read_state) => {
+                match msg_body_read_state {
+                    MsgBodyReadState::FormatType(msg_type) => {
+                        println!("Message type: {msg_type}");
+                        (ReadState::ParseMessage(MsgBodyReadState::Length(byte)), offset + 1)
+                    }
+                    MsgBodyReadState::Length(format_type) => {
+                        println!("Format type: {format_type}");
+                        (ReadState::ParseMessage(MsgBodyReadState::Body(byte)), offset + 1)
+                    }
+                    MsgBodyReadState::Body(msg_length) => {
+                        let msg_end_offset = offset + usize::from(msg_length);
+                        println!("Length: {msg_length}");
+                        if msg_end_offset < buf.len() {
+                            let msg_body = &buf[offset..msg_end_offset];
+                        }
+                        (ReadState::Header(HeaderReadState::Head1), msg_end_offset + 1)
+                    }
                 }
-            },
-            ReadState::ParseMessage => {
-
             }
         }
     }
+
+    println!("Msg count: {msg_counter}. Skipped: {skipped_msgs}")
 }
